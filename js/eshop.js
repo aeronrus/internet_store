5063c6815b4490820dd997a6a58435c25c5ed36d
Ruslan Rogalev <113843437+aeronrus@users.noreply.github.com>
2022-09-24 15:46:15 +0300

    Update eshop.js

--- js/eshop.js
+++ js/eshop.js
@@ -7,14 +7,18 @@ function loadGoods() {
   $.getJSON('goods.json', function (data) {
       let goods = document.getElementById('goods');
       var out = '';
-      for (var key in data) {
-         goods.innerHTML += '<div class="single_goods">';
-         goods.innerHTML += '<h3>' + data[key]['name'] + '</h3>';
-         goods.innerHTML += '<p>Цена: ' + data[key]['cost'] + '</p>';
-         goods.innerHTML += '<img src="' + data[key].image + '" >';
-         goods.innerHTML += '<button data-art="' + key + '">Купить</button>';
-         goods.innerHTML += '</div>';
+   for (var key in data) {
+         out += '<div class="single_goods">';
+         out += '<h3>' + data[key]['name'] + '</h3>';
+         out += '<p>Цена: ' + data[key]['cost'] + '</p>';
+         out += '<img src="' + data[key].image + '" >';
+         out += '<button data-art="' + key + '">Купить</button>';
+         out += '</div>';
       }
+
+      let goods = document.getElementById('goods');
+      goods.insertAdjacentElement("afterbegin", out);
+
    })
 }
-console.log('pizdec2')
+console.log('pizdec3')
